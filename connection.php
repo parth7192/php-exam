b136808329821a2031d016f2b573bade0018c8d7
Parth Vora <parthvora1860@gmail.com>
2024-11-28 18:04:42 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,18 +2,14 @@
 
 class Config    
 {
-
     private $HOST = 'localhost';
     private $USERNAME = 'root';
     private $PASSWORD = '';
     private $DATABASE = "parking";
 
-
-
     public function connect()
     {
         $response = mysqli_connect($this->HOST, $this->USERNAME, $this->PASSWORD, $this->DATABASE); 
         return $response;
-
     }
 }
